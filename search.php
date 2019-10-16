d9ba37559dfae019c7a8464daced0632f6b4d3c3
icarus404 <6576@ait.nsw.edu.au>
2019-10-16 13:05:01 +1100

    shopping cart

--- search.php
+++ search.php
@@ -1,14 +1,6 @@
 <?php
 require('vendor/autoload.php');
-
-//get user's wishlist total
-use bhrsujit\WishList;
-
-$wish = new WishList();
-$wish_total = $wish -> getWishListTotal();
-
 use bhrsujit\Search;
-
 if( isset($_GET['query']) ){
   $search = new Search();
   $result = $search -> getSearchResult();
@@ -17,24 +9,29 @@ else{
   $result = '';
 }
 
+use bhrsujit\WishList;
+$wish_list = new WishList();
+$wish_total = $wish_list -> getWishListTotal();
+use bhrsujit\ShoppingCart;
+$cart = new ShoppingCart();
+$cart_total = $cart -> getCartTotal();
+
 // create navigation
 use bhrsujit\Navigation;
-
 $nav = new Navigation();
 $navigation = $nav -> getNavigation();
-
 //create twig loader for templates
 $loader = new Twig_Loader_Filesystem('templates');
 //create twig environment and pass the loader
 $twig = new Twig_Environment($loader);
 //call a twig template
 $template = $twig -> load('search.twig');
-
 //output the template and pass the data
 echo $template -> render( array(
   'result' => $result,
   'navigation' => $navigation,
+  'wish_count' => $wish_total,
+  'cart_count' => $cart_total,
   'title' => "Search Result for " . $result['query']
 ) );
-
 ?>
